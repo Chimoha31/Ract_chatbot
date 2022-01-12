da156dd3a314708376d4b42feed432e6b6c02a3a
Chimoha31 <mokochii1108@gmail.com>
2022-01-11 19:46:00 -0800

    create state

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./assets/styles/styles.css";
+import defaultDataset from "./dataset";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false
+    };
+  }
+
+  render() {
+    return (
+    <div>
+
+    </div>
+    );
+  }
+}
