baac0f3483c80242489c822cfe120fa1bce83daf
Chimoha31 <mokochii1108@gmail.com>
2022-01-14 01:47:32 -0800

    judge which side chat should stay

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import "./assets/styles/styles.css";
 import defaultDataset from "./dataset";
-import {AnswersList} from './components/index';
+import { AnswersList, Chats } from "./components/index";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,30 +11,45 @@ export default class App extends React.Component {
       chats: [],
       currentId: "init",
       dataset: defaultDataset,
-      open: false
+      open: false,
     };
   }
 
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+    this.setState({
+      answers: initAnswers,
+    });
+  };
 
-initAnswer = () => {
-const initDataset = this.state.dataset[this.state.currentId];
-const initAnswers = initDataset.answers;
-this.setState({
-  answers: initAnswers
-})
-}
+  initChats = () => {
+    const initDataset = this.state.chat[this.state.currentId];
+    const chat = {
+      text: initDataset.question,
+      type: "question",
+    };
+    const chats = this.state.chats;
+    chats.push(chat);
 
-componentDidMount() {
-  this.initAnswer();
-}
+    this.setState({
+      chats: chats
+    });
+  };
+
+  componentDidMount() {
+    this.initChats();
+    this.initAnswer();
+  }
 
   render() {
     return (
-    <section className="c-section">
-      <div className="c-box">
-      <AnswersList answers={this.state.answers} />
-      </div>
-    </section>
+      <section className="c-section">
+        <div className="c-box">
+          <Chats chats={this.state.chats} />
+          <AnswersList answers={this.state.answers} />
+        </div>
+      </section>
     );
   }
 }
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
+// eslint-disable-next-line
 const useStyles = makeStyles((theme) => ({
   root: {},
 }));
